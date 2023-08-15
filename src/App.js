702055a5860496cb834c957c9d2c3e59f2192a80
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2023-08-15 19:31:10 +0200

    UC#8 Unit test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useSelector } from "react-redux";
 import { selectUsers } from "./store/userSlice";
 import ContactForm from "./components/ContactForm/ContactForm";
