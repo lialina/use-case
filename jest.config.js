b2c50e7783b162d224f35759e4aa1da8687f3bf1
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2023-08-15 19:15:35 +0200

    UC#8 Unit test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules",
+  },
+  setupFilesAfterEnv: ["./src/setupTests.js"],
 };
